cc50931b2ccced6862c157af5ae55b34d7d0ec19
bleak <ffeeaarrofdeath@mail.ru>
2024-05-24 09:15:59 +0300

    edit to js synt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	const time = new Date();
 	const year = time.getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{year}
-			</header>
-		</div>
+	const app = createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { className: 'App-logo', src: `${logo}` }),
+			createElement('p', null, 'Edit src/App.js and save to reload.'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('div', null, `${year}`),
+		),
 	);
+
+	return app;
 };
 
 export default App;
